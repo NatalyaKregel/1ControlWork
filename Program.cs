1d30cdfe498867db7083af0acd3ead8848b1d693
Nata Kregel <natik.kregel@gmai.com>
2022-09-28 20:35:16 +0300

    Создаем новый массив из строк не более трех символов

--- Program.cs
+++ Program.cs
@@ -7,3 +7,16 @@
 
 string[]array1 = new string[7] {"hello", "2", "world", ":-)", "88", "yes", "no"};
 string[]array2 = new string[array1.Length];
+
+void SecondArray (string[]array1, string[]array2)
+{
+    int count = 0;
+    for (int i=0; i<array1.Length;i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            array2[count]=array1[i];
+            count++;
+        }
+    }
+}
